d1573193f9ff01233d1e57314dfbd91c14e68bce
Mohit kumar <mohitkr.mca@gmail.com>
2018-04-28 19:59:17 +0530

    frontend changes for php api

---
+++ public/scripts/controllers/signupController.js
@@ -0,0 +1,17 @@
+tabtalent.controller('SignupController', ['$http', '$scope', '$stateParams', '$state', '$rootScope', function ($http, $scope, $stateParams, $state, $rootScope) {
+
+    
+    $scope.registerUser = function () {
+        $http.post('http://localhost/tab_api/index.php/users/signup', $scope.cred).then(function (res) {
+            $rootScope.user = (res.data || {}).data;
+            $rootScope.user.experiences = [];
+            $rootScope.user.skills = [];
+            $rootScope.user.educations = [];
+            $state.go('app.register');
+            localStorage.setItem('TabTalentUser', JSON.stringify($rootScope.user));
+        }, function (error) { 
+            $scope.message = error.data.message;
+        });
+    }
+    
+}]);
---
+++ routes/users.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var User = require('../models/user');
+var Verify = require('./verify');
+var passport = require('passport');
+var ObjectId = require('mongodb').ObjectID;
+
+var userRouter = express.Router();
+
+userRouter.use(bodyParser.json());
+
+
+userRouter.route('/signup')
+
+    .post(function (req, res, next) {
+        User.register(new User({username: req.body.username}), req.body.password, function (err, user) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            User.findOneAndUpdate({_id : user._id}, req.body, {new : true}, function (err, user) {
+                if (err) {
+                    User.remove({username: req.body.username}, function (err, result) {
+                        return res.status(500).send({message: 'A user with the given E-mail is already registered'});
+                    });
+                }
+                passport.authenticate('local')(req, res, function () {
+                    var token = Verify.getToken({"username": user.username, "_id":user._id, "role": user.role});
+
+                    res.status(200).send({
+                        message: 'Login successful',
+                        success: true,
+                        user: user,
+                        token: token
+                    });
+                });
+            })
+        });
+    });
+
+userRouter.route('/login')
+
+    .post(function (req, res, next) {
+        passport.authenticate('local', function (err, user, info) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+
+            if (info) {
+                return res.status(401).send(info);
+            }
+
+            if(!user) {
+                return res.status(401).send(info);
+            }
+
+            req.logIn(user, function (err) {
+                if (err){
+                    return res.status(500).send(err);
+                }
+            });
+            var token = Verify.getToken({"username": user.username, "_id":user._id, "role": user.role});
+
+            res.status(200).send({
+                message: 'Login successful',
+                success: true,
+                user: user,
+                token: token
+            });
+        })(req, res, next);
+    });
+
+userRouter.route('/update')
+
+    .put(function (req, res, next) {
+        console.log(req.body);
+        User.findOneAndUpdate({_id: req.body._id}, req.body, {new : true}, function (err, user) {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.status(200).send(user);
+        })
+    });
+
+
+module.exports = userRouter;
